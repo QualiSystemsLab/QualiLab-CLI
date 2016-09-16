c2787cffc7e48644972b26eac64174210f99ff51
greg-s <gregory.s@qualisystems.com>
2016-09-16 09:15:13 -0700

    Directory rename

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
-setup(name='QualiLiab-CLI', packages=['QualiLab_CLI'], version='1.0.0',
+setup(name='QualiLiab-CLI', packages=['QualiLab-CLI'], version='1.0.0',
       description='Command line library suport Telnet and SSH for use with Quali CloudShell field solutions', author='Gregory Sloan',
       author_email='gregory.s@quali.com',
-      url='https://github.com/QualiSystemsLab/QualiLab-CLI/QualiLab_CLI',
-      download_url='https://github.com/QualiSystemsLab/Quali-Lab-CLI/QualiLab_CLI/tarball/1.0.0',
+      url='https://github.com/QualiSystemsLab/QualiLab-CLI',
+      download_url='https://github.com/QualiSystemsLab/QualiLab-CLI/tarball/1.0.0',
       keywords=['telnet', 'console', 'handler', 'ssh','cli','quali', 'cloudshell'], classifiers=[],
       )

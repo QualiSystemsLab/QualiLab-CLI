550fa4c63b2717f9ae08872e97fe257024ce28cb
greg-s <gregory.s@qualisystems.com>
2016-09-16 09:30:20 -0700

    tag update in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
-setup(name='QualiLab_CLI', packages=['QualiLab_CLI'], version='1.0.1',
+setup(name='QualiLab_CLI', packages=['QualiLab_CLI'], version='1.0.2',
       description='Command line library suport Telnet and SSH for use with Quali CloudShell field solutions', author='Gregory Sloan',
       author_email='gregory.s@quali.com',
       url='https://github.com/QualiSystemsLab/QualiLab_CLI',
-      download_url='https://github.com/QualiSystemsLab/QualiLab_CLI/tarball/1.0.1',
+      download_url='https://github.com/QualiSystemsLab/QualiLab_CLI/tarball/1.0.2',
       keywords=['telnet', 'console', 'handler', 'ssh','cli','quali', 'cloudshell'], classifiers=[],
       )
